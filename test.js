32a8122d0222e0e01e6839c3931ad349aaa17416
iamishaqkhan <ishaqpakkaya@gmail.com>
2024-09-03 22:28:07 +0500

    variable

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("ishaq");
+
+ 
+
+ const accountId = 123456
+accountcity = "swabi"
+accountPassword = "3434"
+accountEmail = "ishaq@gmail.com"
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountId, accountcity] );
